f00057533f08aef279959133ea5a6a6d2c14a27f
dudeofa <andrew.langhill@gmail.com>
2020-07-25 15:13:52 -0400

    patch

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
+const { check, validationResult } = require('express-validator');
+
 const { readFile } = require('fs').promises;
 
 const mcQuery = require('./mcQuery.js');
 
 const app = express();
 app.use(bodyParser.json());
-
-if (process.env.NODE_ENV !== 'production') {
-    const dotenv = require('dotenv');
-    dotenv.config();
-}
 
 // Root directory
 app.get('/', async (request, response) => {
@@ -20,36 +17,35 @@ app.get('/', async (request, response) => {
 
 // MC JSON get
 app.get('/mc', async (request, response) => {
-
-    if (request.query.ip !== undefined) {
-        console.log("[MC] Querying from query " + request.query.ip + ":" + request.query.port + "...")
+    
+    if (typeof request.query !== 'undefined') {
+        console.log("Querying " + request.query.ip + ":" + request.query.port + "...")
 
         await mcQuery(request.query.ip, request.query.port)
         .then((server) => response.send(server))
         .catch((err) => response.send(err));
         
-    } else if (request.body.ip !== undefined) {
-        console.log("[MC] Querying from body " + request.body.ip + ":" + request.body.port + "...")
+    } else if (typeof request.body !== 'undefined') {
+        console.log("Querying " + request.body.ip + ":" + request.body.port + "...")
 
         await mcQuery(request.body.ip, request.body.port)
         .then((server) => response.send(server))
         .catch((err) => response.send(err));
 
     } else {
-        response.send(await readFile('./mc.html', 'utf8'));
+        console.log("Empty request made by " + request.ip)
+        response.send("No info supplied :(")
     }
     
 });
 
 // MC webpage get
 app.use('/mc/:ip', async (request, response) => {
-    console.log("[MC] Querying " + request.params.ip + "...")
+    console.log("Querying " + request.params.ip + "...")
 
     await mcQuery(request.params.ip)
     .then((server) => response.send(server))
     .catch((err) => response.send(err));
 })
 
-var server = app.listen(process.env.KYPORT || 3000, () => {
-    console.log(`App available on https://api.kylixor.com on port ` + server.address().port);
-});
+app.listen(process.env.KYPORT || 3000, () => console.log(`App available on https://api.kylixor.com`));
